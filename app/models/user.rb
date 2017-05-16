72cde536d7e268a32d3f74a2f429b9c0c356860c
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-16 13:38:31 -0700

    Implement User::find_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+  
 end
