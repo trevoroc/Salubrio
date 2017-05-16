2c3cfe1e27c87368a6899a7c23ebe9e7b0a8c250
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-16 13:45:20 -0700

    Add BCrypt gem, implement User#password= and User#is_password?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  before_validation :ensure_session_token
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil if user.nil?
@@ -11,5 +13,17 @@ class User < ApplicationRecord
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  def is_password?(password)
+    real_password = BCrypt::Password.new(self.password_digest)
+    real_password.is_password?(password)
   end
 end
