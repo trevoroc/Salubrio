84a4d60dbff5e8e879320c65818bfdb7bd798f79
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-16 15:47:24 -0700

    Implement basic SessionReducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,13 @@
+import { createUser, RECEIVE_USER } from '../actions/session_actions';
+
+const SessionReducer = (state, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_USER:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
--- frontend/salubrio.jsx
+++ frontend/salubrio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // TODO: Code below this line for testing only
-import { createUser } from './actions/user_actions';
+import { createUser } from './actions/session_actions';
 window.createUser = createUser;
 // TODO: Code aboe this line for testing only
 
