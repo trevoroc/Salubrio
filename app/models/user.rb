f8083e86b437c7d02458cb69d12d60135552ac1a
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-16 13:35:25 -0700

    Auth: Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+end
