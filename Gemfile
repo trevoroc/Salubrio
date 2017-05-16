e34152031f25b6ca885a5afe4078a08921840ccb
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-16 14:12:51 -0700

    Implement users#create and users#update actions, update Gemfile

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ group :development do
   gem 'binding_of_caller'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'listen', '~> 3.0.5'
+  gem 'pry-rails'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,27 @@
 class Api::UsersController < ApplicationController
 
+  def create
+    @user = User.create(user_params)
+    if @user.save!
+      login(@user)
+      render :show # TODO: May need to update this later
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render :show # TODO: May need to update this later
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password) # TODO: May need to add more later
+  end
 end
