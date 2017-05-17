952b9ce86cb4998d5c2ddf1ff1a41ca6e09ec663
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-17 10:36:19 -0700

    Implement route utils for Auth and Protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 const App = () => (
   <div>
     <h1>Welcome to Salubrio</h1>
-    // <Route exact path='/signup' component={ Auth }
+    <AuthRoute exact path='/signup' component={ Auth } />
   </div>
 );
 
