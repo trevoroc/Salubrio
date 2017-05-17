89f0a751f93983c222aa5248829e6f82628f0860
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-17 12:04:52 -0700

    Implement basic auth form in browser

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import AuthFormContainer from './sessions/auth_form_container';
@@ -7,7 +7,11 @@ import AuthFormContainer from './sessions/auth_form_container';
 const App = () => (
   <div>
     <h1>Welcome to Salubrio</h1>
-    <AuthRoute exact path='/signup' component={ AuthFormContainer } />
+    <AuthRoute exact path="/signup" component={ AuthFormContainer } />
+    <AuthRoute exact path="/login" component={ AuthFormContainer } />
+    <Link to="/signup">Sign Up</Link>
+    <br></br>
+    <Link to="/login">Log In</Link>
   </div>
 );
 
--- frontend/components/sessions/auth_form.jsx
+++ frontend/components/sessions/auth_form.jsx
@@ -9,10 +9,18 @@ class AuthForm extends React.Component {
     };
 
     this.update = this.update.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   update(property) {
     return e => this.setState({ [property]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.sendForm(this.state);
+    // TODO: Push new page to history?
   }
 
   render() {
@@ -29,11 +37,15 @@ class AuthForm extends React.Component {
         <input type="text" placeholder="Username"
           onChange={ this.update('username') }
           value={ this.state.username }></input>
+
         <input type="password" placeholder="Password"
           onChange={ this.update('password') }
           value={ this.state.password }></input>
+
         <button onClick={ this.handleSubmit }></button>
       </form>
     );
   }
 }
+
+export default AuthForm;
