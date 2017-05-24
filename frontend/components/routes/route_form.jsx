8b4f9ab75204dbc1b3c48c1f5b3111bbc31ab0b3
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-24 16:23:49 -0700

    Render map directions

--- frontend/components/routes/route_form.jsx
+++ frontend/components/routes/route_form.jsx
@@ -4,12 +4,54 @@ class RouteForm extends React.Component {
   constructor(props) {
     super(props);
     this.map = null;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
 
     this.state = {
       waypoints: []
     };
 
+    this.initMap = this.initMap.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleDirections = this.handleDirections.bind(this);
+  }
+
+  buildWaypoints() {
+    return (
+      this.state.waypoints.slice(1, this.state.waypoints.length - 1)
+        .map(wp => ({
+          location: { lat: wp.lat(), lng: wp.lng() },
+          stopover: true
+        }))
+    );
+  }
+
+  buildRequest() {
+    const origin = this.state.waypoints[0];
+    const destination = this.state.waypoints[this.state.waypoints.length - 1];
+    const waypoints = this.buildWaypoints();
+
+    return {
+      origin,
+      destination,
+      waypoints,
+      travelMode: 'WALKING',
+      optimizeWaypoints: false
+    };
+  }
+
+  handleDirections(result, status) {
+    // console.log('Received result');
+    if (status === 'OK') {
+      this.directionsDisplay.setDirections(result);
+    }
+  }
+
+  calculateRoute() {
+    if (this.state.waypoints.length > 1) {
+      // console.log('About to send request');
+      this.directionsService.route(this.buildRequest(), this.handleDirections);
+    }
   }
 
   handleClick(e) {
@@ -19,15 +61,26 @@ class RouteForm extends React.Component {
     });
 
     this.state.waypoints.push(e.latLng);
+    this.calculateRoute();
+  }
+
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.initMap(position.coords.latitude, position.coords.longitude);
+      this.map.addListener('click', this.handleClick);
+    });
+  }
+
+  initMap(lat, lng) {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat, lng },
+      zoom: 12
+    });
+    this.directionsDisplay.setMap(this.map);
   }
 
   componentDidMount() {
-    this.map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
-    });
-
-    this.map.addListener('click', this.handleClick);
+    this.getCurrentLocation();
   }
 
   render() {
