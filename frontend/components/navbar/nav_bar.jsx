533379fc5f3873af2731cadd44721f4d33b177cc
Trevor O'Connor <trevoroc22@gmail.com>
2017-05-26 15:45:56 -0700

    Add link to reach route form

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -30,7 +31,7 @@ class NavBar extends React.Component {
             <button className="dropdown-link">Add a workout</button>
           </li>
           <li key="route">
-            <button className="dropdown-link">Create a route</button>
+            <Link to="/newroute" className="dropdown-link">Create a route</Link>
           </li>
           <li key="profile">
             <button className="dropdown-link">Profile</button>
@@ -44,34 +45,5 @@ class NavBar extends React.Component {
     );
   }
 }
-
-// const NavBar = props => (
-//   <header className="nav">
-//     <h1>Salubrio</h1>
-//     <button onClick={ e => {
-//         e.preventDefault();
-//         const dropdown = document.querySelector('.dropdown');
-//         dropdown.classList.toggle('hidden');
-//       }}>{ props.username }</button>
-//     <ul className="dropdown hidden">
-//       <li key="workout">
-//         <button className="dropdown-link">Add a workout</button>
-//       </li>
-//       <li key="route">
-//         <button className="dropdown-link">Create a route</button>
-//       </li>
-//       <li key="profile">
-//         <button className="dropdown-link">Profile</button>
-//       </li>
-//       <li key="logout">
-//         <button onClick={ e => {
-//             e.preventDefault();
-//             props.logout();
-//           }
-//         } className="dropdown-link">Logout</button>
-//       </li>
-//     </ul>
-//   </header>
-// );
 
 export default NavBar;
