04a582294f974b45da146e2b9a69109307f51d8f
Trevor O'Connor <trevoroc22@gmail.com>
2017-06-20 11:13:30 -0700

    Refactor SplashHeaderContainer into the Splash component

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SplashHeaderContainer from './splash_header_container';
+
+const Splash = () => {
+  return (
+    <div>
+      <SplashHeaderContainer />
+    </div>
+  );
+};
+
+export default Splash;
